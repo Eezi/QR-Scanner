06916e232c6c5096347a09de6b1a33101ebdc112
Eezi <tomi.a.salo@outlook.com>
2020-03-09 11:45:00 +0200

    QR-Scannerin toimiva versio

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,69 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: HomePage(),
-));
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
-class HomePage extends StatelessWidget {
-  // This widget is the root of your application.
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-     appBar: AppBar(
-       title: Text('QR Scanner'),
-      centerTitle: true,   
-     ),
-     body: Center(
-       child: Text('Hey there', 
-       style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-       ),
-    ),
-    floatingActionButton: FloatingActionButton.extended(
-      icon: Icon(Icons.camera_alt),
-      label: Text("Scan"),
-      onPressed: (){},
-    ),
-    floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
+  HomePageState createState() {
+    return new HomePageState();
   }
 }
 
+class HomePageState extends State<HomePage> {
+  String result = "Hey there !";
+
+  Future _scanQR() async {
+    try {
+      String qrResult = await BarcodeScanner.scan();
+      setState(() {
+        result = qrResult;
+      });
+    } on PlatformException catch (ex) {
+      if (ex.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          result = "Camera permission was denied";
+        });
+      } else {
+        setState(() {
+          result = "Unknown Error $ex";
+        });
+      }
+    } on FormatException {
+      setState(() {
+        result = "You pressed the back button before scanning anything";
+      });
+    } catch (ex) {
+      setState(() {
+        result = "Unknown Error $ex";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Scanner"),
+      ),
+      body: Center(
+        child: Text(
+          result,
+          style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.camera_alt),
+        label: Text("Scan"),
+        onPressed: _scanQR,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
